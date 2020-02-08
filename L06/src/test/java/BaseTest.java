cb1ffd7731f07552f1ee6d8fdb5d009ca1971635
andrey-gilmiyarov <andreygilmiyarov@mail.ru>
2020-02-08 13:45:27 +0300

    L06 - v3

--- L06/src/test/java/BaseTest.java
+++ L06/src/test/java/BaseTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
@@ -14,16 +13,11 @@ public class BaseTest {
     protected WebDriver driver;
     protected WebDriverWait webDriverWait;
 
-    @BeforeClass
-    public static void setUpClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
     @BeforeTest
     public void setupTest() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        webDriverWait = new WebDriverWait(driver, 10);
+        webDriverWait = new WebDriverWait(driver, 15);
         logger.info("Setup is completed.");
     }
 
--- L06/src/test/java/YandexMarketTest.java
+++ L06/src/test/java/YandexMarketTest.java
@@ -2,6 +2,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
@@ -25,9 +26,9 @@ public class YandexMarketTest extends BaseTest {
 
         driver.get("https://market.yandex.ru/");
 //        driver.get("https://market.yandex.ru/catalog--mobilnye-telefony/54726/list?hid=91491&track=pieces");
-//        driver.get("https://market.yandex.ru/catalog--mobilnye-telefony/54726/list?hid=91491&glfilter=7893318%3A7701962%2C459710&glfilter=16816262%3A16816264&local-offers-first=0&onstock=1&how=aprice");
+//        driver.get("https://market.yandex.ru/catalog--mobilnye-telefony/54726/list?hid=91491&glfilter=16816262%3A16816264&local-offers-first=0&onstock=1&how=aprice");
 //        driver.get("https://market.yandex.ru/compare/3jts68QxiMH4WuzW5TFQn9MQaTx?hid=91491&id=401246433&id=217969159");
-
+        logger.info("Зашли на сайт");
 
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("27903768-tab")));
         driver.manage().addCookie(new Cookie("yandex_gid", cookie1.getValue()));
@@ -37,20 +38,26 @@ public class YandexMarketTest extends BaseTest {
         driver.findElement(By.id("27903768-tab")).click();
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[href='/catalog--mobilnye-telefony/54726/list?hid=91491']")));
         driver.findElement(By.cssSelector("a[href='/catalog--mobilnye-telefony/54726/list?hid=91491']")).click();
+        logger.info("Зашли в раздел \"Мобильные телефоны\"");
 
         //выбираем только смартфоны и фильтруем по цене
         driver.findElement(By.cssSelector("fieldset[data-autotest-id='16816262'] > ul > li:last-child > div > label")).click();
         driver.findElement(By.linkText("по цене")).click();
+        logger.info("Применили фильтры \"по цене\" и \"смартфон\"");
 
         //добавляем в сравнение
         addToComparison(BrandForComparison.HUAWEI);
         filterByBrand(BrandForComparison.HUAWEI);
         addToComparison(BrandForComparison.XIAOMI);
 
-//        переходим в сравнение и проверяем что элементов 2
-        driver.findElement(By.cssSelector(".popup-informer__controls > a ")).click();
+//        переходим в сравнение
+        driver.findElement(By.cssSelector("a[href='/compare?track=head']")).click();
+        logger.info("Перешли в сравнение");
+
+        //проверяем что элементов 2
         List<WebElement> comparisonElem = driver.findElements(By.cssSelector(".n-compare-content__line.i-bem.n-compare-content__line_js_inited > div"));
         Assert.assertEquals(comparisonElem.size(), 2);
+        logger.info("Проверили что элементов в сравнение 2");
 
         //открываем "все характеристики" и проверяем что поле "ОПЕРАЦИОННАЯ СИСТЕМА" присутствует
         driver.findElement(By.cssSelector("span.link.n-compare-show-controls__all")).click();
@@ -59,7 +66,7 @@ public class YandexMarketTest extends BaseTest {
             String rowName = compareRowName.getText();
             if (rowName.equalsIgnoreCase("ОПЕРАЦИОННАЯ СИСТЕМА")) {
                 Assert.assertEquals(rowName.toUpperCase(), "ОПЕРАЦИОННАЯ СИСТЕМА");
-                logger.info("Найдено поле \"ОПЕРАЦИОННАЯ СИСТЕМА\"");
+                logger.info("Во вкладке \"все характеристики\" найдено поле \"ОПЕРАЦИОННАЯ СИСТЕМА\"");
             }
         }
 
@@ -72,8 +79,10 @@ public class YandexMarketTest extends BaseTest {
                 logger.error("Во вкладке \"различающиеся характеристики\" найдено поле \"ОПЕРАЦИОННАЯ СИСТЕМА\"!");
             Assert.assertNotEquals(rowName.toUpperCase(), "ОПЕРАЦИОННАЯ СИСТЕМА");
         }
+        logger.info("Во вкладке \"различающиеся характеристики\" поле \"ОПЕРАЦИОННАЯ СИСТЕМА\" отсутствует");
 
-        Thread.sleep(1000);
+        logger.info("Тест пройден");
+//        Thread.sleep(1000);
 
     }
 
@@ -87,20 +96,31 @@ public class YandexMarketTest extends BaseTest {
                 break;
             }
         }
+        logger.info("Применили фильтрацию по бренду " + brandForComparison.toString());
         Thread.sleep(3000);
     }
 
     private void addToComparison(BrandForComparison brandForComparison) throws InterruptedException {
         if (!brandForComparison.isCompareToThisBrand()) {
             filterByBrand(brandForComparison);
-            //не получаеся подождать подгрузку элементов после фильтрации
-//            webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(":nth-child(1) > .n-snippet-cell2__hover > div > div > div")));
+//            boolean isCompare = false;
+//            while (!isCompare) {
+//                try {
+//                    driver.findElement(By.cssSelector(":nth-child(1) > .n-snippet-cell2__hover > div > div > div")).click();
+//                    isCompare = true;
+//                } catch (StaleElementReferenceException ex) {
+//                    logger.info("Кнопка \"Добавить к сравнению\" пока не появилась на странице");
+//                }
+//            }
+//            webDriverWait.until(ExpectedConditions.elementToBeClickable(By.className(":nth-child(1) > .n-snippet-cell2__hover > div > div > div")));
+//            webDriverWait.until(ExpectedConditions.elementToBeClickable(By.className(":nth-child(1) > .n-snippet-cell2__header > .n-snippet-cell2__title > a")));
             driver.findElement(By.cssSelector(":nth-child(1) > .n-snippet-cell2__hover > div > div > div")).click();
             webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.className("popup-informer__title")));
             String popupTitle = driver.findElement(By.className("popup-informer__title")).getText();
             Assert.assertTrue(popupTitle.toUpperCase().contains(brandForComparison.toString()));
             brandForComparison.setCompareToThisBrand(true);
             driver.findElement(By.cssSelector(".popup-informer__close.image.image_name_close")).click();
+            logger.info("Смартфон бренда " + brandForComparison.toString() + " добавлен в сравнение");
         }
     }
 
